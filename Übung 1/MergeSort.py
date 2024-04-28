564eaa060bafc752fb7be36f52edcb4cd229b141
Hitchhock <75169220+Hitchhock@users.noreply.github.com>
2024-04-28 17:39:48 +0200

    Stylecheck, Makefile etc.

--- Übung 1/MergeSort.py
+++ Übung 1/MergeSort.py
@@ -2,6 +2,9 @@
     Ayiza Muzaffar - am819 - 5136283
     Berthold Dominique Buri - bb268 - 4744252
     Henry Juncker - hj84 - 5605330
+
+    falls der Plot neu erstellt werden soll,
+    entkommentieren sie Zeile 125
 """
 
 
@@ -12,20 +15,20 @@ import time
 from matplotlib import pyplot as plt
 
 
-def SplitArrayIntoEvenParts (a, n):
+def split_array_into_even_parts(a, n):
     """
         Teilt einen Array a in n ca. gleich große Teile auf
-        * Hab ich so von StackOverflow, kann man evtl. noch durch eigenen Code ersetzen
     """
     k, m = divmod(len(a), n)
     return list(a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
 
-def MergeTwoArrays (arr1, arr2):
+
+def merge_two_arrays(arr1, arr2):
     """
         Zusammenführen und Sortieren von zwei Arrays.
         Gleicher Aufbau wie in der Vorlesung.
     """
-    mergedArr = []
+    merged_arr = []
     pos = 0
     i = 0
     j = 0
@@ -34,17 +37,18 @@ def MergeTwoArrays (arr1, arr2):
 
     while pos < len1 + len2:
         if i < len1 and (j >= len2 or arr1[i] < arr2[j]):
-            mergedArr.append (arr1[i])
+            merged_arr.append(arr1[i])
             pos += 1
             i += 1
         else:
-            mergedArr.append (arr2[j])
+            merged_arr.append(arr2[j])
             pos += 1
             j += 1
-    
-    return mergedArr
 
-def MergeSortRecursive (arr, k):
+    return merged_arr
+
+
+def merge_sort_recursive(arr, k):
     """
        Perform merge sort on a list, splitting it into k parts.
 
@@ -56,62 +60,66 @@ def MergeSortRecursive (arr, k):
            list: The sorted list.
 
        Examples:
-           >>> MergeSortRecursive([12, 11, 13, 5, 6, 7], 3)
+           >>> merge_sort_recursive([12, 11, 13, 5, 6, 7], 3)
            [5, 6, 7, 11, 12, 13]
-           >>> MergeSortRecursive([], 10)
+           >>> merge_sort_recursive([], 10)
            []
-           >>> MergeSortRecursive([9], 10)
+           >>> merge_sort_recursive([9], 10)
            [9]
     """
     n = len(arr)
     if n <= 1:
         return arr
-    
-    splitArr = SplitArrayIntoEvenParts(arr, k)
-    sortedArr = []
+
+    split_arr = split_array_into_even_parts(arr, k)
+    sorted_arr = []
 
     # Iterration durch die k Teile des Arrays um
     # * Rekursivität zu ermöglichen und
-    # * den sortedArray immer im direkten Vergleich mit dem nächsten Split vom Array zu mergen und sortieren.
-    for elem in splitArr:
-        elem = MergeSortRecursive(elem, k)
-        sortedArr = MergeTwoArrays(sortedArr, elem)
-    
-    return sortedArr
+    # * den sortedArray immer im direkten Vergleich
+    #   mit dem nächsten Split vom Array zu mergen und sortieren.
+    for elem in split_arr:
+        elem = merge_sort_recursive(elem, k)
+        sorted_arr = merge_two_arrays(sorted_arr, elem)
+
+    return sorted_arr
 
 
-# testing some random arrays to help you check correctness
-if __name__ == "__main__":
-    listN = np.arange(100, 10001, 1000)
-    valuesK = {}
+def create_plot():
+    list_n = np.arange(100, 10001, 1000)
+    values_k = {}
 
-    for n in listN:
+    for n in list_n:
         print(n)
-        listK = [2, 3, int(math.log2(n)), n//4]
+        list_k = [2, 3, int(math.log2(n)), n//4]
         arr = [random.randint(0, 10000) for i in range(n)]
 
-        for idx, k in enumerate(listK):
+        for idx, k in enumerate(list_k):
             anfang = time.perf_counter()
-            MergeSortRecursive(arr, k)
-            if idx not in valuesK:
-                valuesK[idx] = []
-            valuesK[idx].append((time.perf_counter() - anfang).total_seconds())
-    
-    listLabel = ["k=2", "k=3", "k=log2(n)", "k=n/4"]
-    for k in valuesK:
-        plt.plot(listN, valuesK[k], label=listLabel[k])
+            merge_sort_recursive(arr, k)
+            if idx not in values_k:
+                values_k[idx] = []
+            values_k[idx].append(time.perf_counter() - anfang)
+
+    list_label = ["k=2", "k=3", "k=log2(n)", "k=n/4"]
+    for k in values_k:
+        plt.plot(list_n, values_k[k], label=list_label[k])
     plt.legend()
     plt.show()
     print("fertig")
 
 
-    """
+# testing some random arrays to help you check correctness
+if __name__ == "__main__":
     n = 1000
     k_limit = 20
     for k in range(2, k_limit):
         arr = [random.randint(0, 10000) for i in range(n)]
-        sorted_arr = MergeSortRecursive(arr, k)
+        sorted_arr = merge_sort_recursive(arr, k)
         if not sorted_arr == sorted(arr):
             raise Exception("Didn't Sort Correctly!")
     print("seems to work")
-    """
+
+    # falls der Plot neu erstellt werden soll,
+    # entkommentieren sie die nächste Zeile
+    # create_plot()
